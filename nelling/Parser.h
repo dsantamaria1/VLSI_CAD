f9909a24bde553fc14df4637b4d22499eca9e183
danielsantamaria <santamaria@utexas.edu>
2016-11-07 22:30:51 -0600

    added c++11 build flag

--- nelling/Parser.h
+++ nelling/Parser.h
@@ -12,8 +12,8 @@ public:
 	void redefine_parser(const string& _filepath, const string& _benchmark);
 private:
 	vector<Net> parse_netlist();
-	vector<vector<Cell> > parse_placement();
-	vector<vector<Site> > parse_sitemap();
+	vector<vector<Cell>> parse_placement();
+	vector<vector<Site>> parse_sitemap();
 	string netsPath;
 	string plPath;
 	string smPath;
